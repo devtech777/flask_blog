82e1f89b48a8f4452d6fb8a317f1fdcdd7c9ea91
Roman N <devtech777@gmail.com>
2020-09-30 10:50:57 +0300

    Initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config():
+    SECRET_KEY = 'a9219274d709e020982eab9a9f5cfd88'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
